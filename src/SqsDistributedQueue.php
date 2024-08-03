b6bdd0f28f058ae9a577e7f6181c73041edb4f7c
Arif <arif@mamiteam.com>
2024-08-03 22:09:08 +0700

    support receive multiple messages and without topic

--- src/SqsDistributedQueue.php
+++ src/SqsDistributedQueue.php
@@ -5,16 +5,37 @@ use Illuminate\Queue\SqsQueue;
 
 class SqsDistributedQueue extends SqsQueue
 {
+    private $messageBuffer = [];
+
     public function pop($queue = null)
     {
-        $response = $this->sqs->receiveMessage([
-            'QueueUrl' => $queue = $this->getQueue($queue),
-            'AttributeNames' => ['ApproximateReceiveCount'],
-        ]);
+        $maxReceiveMessage = 1;
+        if (! config('queue.connections.sqs-distributed.use_topic', true)) {
+            // only allow batch operation when message not using topic format
+            $maxReceiveMessage = config('queue.connections.sqs-distributed.max_receive_message', 1);
+        }
 
-        if (! is_null($response['Messages']) && count($response['Messages']) > 0) {
+        if (empty($this->messageBuffer)) {
+            $response = $this->sqs->receiveMessage([
+                'QueueUrl' => $queue = $this->getQueue($queue),
+                'AttributeNames' => ['ApproximateReceiveCount'],
+                'MaxNumberOfMessages' => $maxReceiveMessage,
+            ]);
+
+            if (! is_null($response['Messages']) && count($response['Messages']) > 0) {
+                if (count($response['Messages']) === 1) {
+                    return new SqsDistributedJob(
+                        $this->container, $this->sqs, $response['Messages'][0],
+                        $this->connectionName, $queue
+                    );
+                } else {
+                    $this->messageBuffer = $response['Messages'];
+                }
+            }
+        }
+        if (! empty($this->messageBuffer)) {
             return new SqsDistributedJob(
-                $this->container, $this->sqs, $response['Messages'][0],
+                $this->container, $this->sqs, array_shift($this->messageBuffer),
                 $this->connectionName, $queue
             );
         }
